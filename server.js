74d868bfb8ef0a35753b1af31dd6fbbbf36eae2b
GustaveNimant <achadde@iabloko.fecamp>
2019-07-03 18:05:59 +0200

    this is a sample!

---
+++ server.js
@@ -0,0 +1,23 @@
+// server.js
+
+// BASE SETUP
+// ==============================================
+
+var express = require('express');
+var app     = express();
+var port    = 	process.env.PORT || 8080;
+
+// ROUTES
+// ==============================================
+
+// sample route with a route the way we're used to seeing it
+app.get('/sample', function(req, res) {
+	res.send('this is a sample!');	
+});
+
+// we'll create our routes here
+
+// START THE SERVER
+// ==============================================
+app.listen(port);
+console.log('Magic happens on port http://localhost:' + port);
