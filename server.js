bedd3ede2884cfa2afe83e0328c24666dbc953d7
GustaveNimant <achadde@iabloko.fecamp>
2019-07-03 18:35:20 +0200

    middleware req.method is  GET req.url is /

--- server.js
+++ server.js
@@ -20,6 +20,16 @@ app.get('/sample', function(req, res) {
 // get an instance of router
 var router = express.Router();
 
+// route middleware that will happen on every request
+router.use(function(req, res, next) {
+ 
+    // log each request to the console
+    console.log('req.method is ',req.method, 'req.url is', req.url);
+ 
+    // continue doing what we were doing and go to the route
+    next(); 
+});
+
 // home page route (http://localhost:8080)
 router.get('/', function(req, res) {
     res.send('I am the home page!');  
