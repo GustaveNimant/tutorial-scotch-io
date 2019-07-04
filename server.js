26bee419f80ffe38cb1b4d4eb8bc774c35fdae98
GustaveNimant <achadde@iabloko.fecamp>
2019-07-04 08:38:04 +0200

    parameter middleware  hello holly

--- server.js
+++ server.js
@@ -40,9 +40,22 @@ router.get('/about', function(req, res) {
     res.send('I am the about page!'); 
 });
 
+// route middleware to validate :name
+router.param('name', function(req, res, next, name) {
+    // do validation on name here
+    // blah blah validation
+    // log something so we know its working
+    console.log('doing name validations on ' + name);
+
+    // once validation is done save the new item in the req
+    req.name = name;
+    // go to the next thing
+    next(); 
+});
+
 // route with parameters (http://localhost:8080/hello/:name)
 router.get('/hello/:name', function(req, res) {
-    res.send('hello ' + req.params.name + '!');
+    res.send('hello ' + req.name + '!');
 });
 
 // apply the routes to our application
