c7b170741dc8894a2dcbe2feba900a5ef0557ac1
GustaveNimant <achadde@iabloko.fecamp>
2019-07-04 08:52:46 +0200

    app.route .get and .post login

--- server.js
+++ server.js
@@ -58,6 +58,22 @@ router.get('/hello/:name', function(req, res) {
     res.send('hello ' + req.name + '!');
 });
 
+// ROUTES
+// ==============================================
+
+app.route('/login')
+
+    // show the form (GET http://localhost:8080/login)
+    .get(function(req, res) {
+        res.send('this is the login form');
+    })
+
+    // process the form (POST http://localhost:8080/login)
+    .post(function(req, res) {
+        console.log('processing');
+        res.send('processing the login form!');
+    });
+
 // apply the routes to our application
 app.use('/', router);
 
